bf15589daca63ea88e1d479800b23d21728955ee
nicomellon <nmellon94@gmail.comm>
2022-02-06 11:59:18 +0100

    refactor osc to separate file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import { useEffect } from 'react';
-import { StyleSheet, SafeAreaView, NativeEventEmitter } from 'react-native';
-import Osc from 'react-native-osc';
+import { StyleSheet, SafeAreaView } from 'react-native';
 import { TransportProviderWrapper } from './context/transport.context';
 import Transport from './components/transport/Transport';
-
-//OSC server IP address and port
-const SEND_ADDRESS = '192.168.1.57';
-const SEND_PORT = 8000;
-const LISTEN_PORT = 9000;
+import createOscConnection from './osc/osc';
 
 export default function App() {
   useEffect(() => {
-    //create an event emiter sending the native osc module as parameter
-    const eventEmitter = new NativeEventEmitter(Osc);
-
-    //subscribe to GotMessage event to receive OSC messages
-    eventEmitter.addListener('GotMessage', (oscMessage) =>
-      console.log(oscMessage)
-    );
-
-    //create the client & server
-    Osc.createClient(SEND_ADDRESS, SEND_PORT);
-    Osc.createServer(LISTEN_PORT);
+    createOscConnection();
   }, []);
 
   return (
---
+++ src/osc/osc.js
@@ -0,0 +1,21 @@
+import { NativeEventEmitter } from 'react-native';
+import Osc from 'react-native-osc';
+
+export default function createOscConnection() {
+  //OSC server IP address and port
+  const SEND_ADDRESS = '192.168.1.57';
+  const SEND_PORT = 8000;
+  const LISTEN_PORT = 9000;
+
+  //create an event emiter sending the native osc module as parameter
+  const eventEmitter = new NativeEventEmitter(Osc);
+
+  //subscribe to GotMessage event to receive OSC messages
+  eventEmitter.addListener('GotMessage', (oscMessage) =>
+    console.log(oscMessage)
+  );
+
+  //create the client & server
+  Osc.createClient(SEND_ADDRESS, SEND_PORT);
+  Osc.createServer(LISTEN_PORT);
+}
