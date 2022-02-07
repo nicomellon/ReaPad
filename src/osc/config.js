d8faf0867420614f6cb5d7e113a8b9359d868a70
nicomellon <nmellon94@gmail.comm>
2022-02-07 19:12:36 +0100

    set osc ip address to localhost

--- src/osc/config.js
+++ src/osc/config.js
@@ -1,4 +1,4 @@
 //OSC server IP address and port
-export const SEND_ADDRESS = '192.168.1.136';
+export const SEND_ADDRESS = 'localhost';
 export const SEND_PORT = 8000;
 export const LISTEN_PORT = 9000;
