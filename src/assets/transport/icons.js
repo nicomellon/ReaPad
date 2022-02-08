248992a69e8bcc4f74736b221fe98b1f7d490342
nicomellon <nmellon94@gmail.com>
2022-02-08 20:49:49 +0100

    functional refactor to redux toolkit

---
+++ src/assets/transport/icons.js
@@ -0,0 +1,58 @@
+export const home = {
+  default: require('./default/transport_home.png'),
+  pressed: require('./pressed/transport_home.png'),
+};
+
+export const end = {
+  default: require('./default/transport_end.png'),
+  pressed: require('./pressed/transport_end.png'),
+};
+
+export const stop = {
+  default: require('./default/transport_stop.png'),
+  pressed: require('./pressed/transport_stop.png'),
+};
+
+export const record = {
+  inactive: {
+    default: require('./default/transport_record.png'),
+    pressed: require('./pressed/transport_record.png'),
+  },
+  active: {
+    default: require('./default/transport_record_on.png'),
+    pressed: require('./pressed/transport_record_on.png'),
+  },
+};
+
+export const play = {
+  inactive: {
+    default: require('./default/transport_play.png'),
+    pressed: require('./pressed/transport_play.png'),
+  },
+  active: {
+    default: require('./default/transport_play_on.png'),
+    pressed: require('./pressed/transport_play_on.png'),
+  },
+};
+
+export const repeat = {
+  inactive: {
+    default: require('./default/transport_repeat_off.png'),
+    pressed: require('./pressed/transport_repeat_off.png'),
+  },
+  active: {
+    default: require('./default/transport_repeat_on.png'),
+    pressed: require('./pressed/transport_repeat_on.png'),
+  },
+};
+
+export const pause = {
+  inactive: {
+    default: require('./default/transport_pause.png'),
+    pressed: require('./pressed/transport_pause.png'),
+  },
+  active: {
+    default: require('./default/transport_pause_on.png'),
+    pressed: require('./pressed/transport_pause_on.png'),
+  },
+};
