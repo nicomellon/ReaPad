73dd7c2691e5db949fadddb5995fd6d79c3e98d7
nicomellon <nmellon94@gmail.comm>
2022-02-08 12:17:38 +0100

    icons without feedback working

--- src/components/transport/Transport.jsx
+++ src/components/transport/Transport.jsx
@@ -1,21 +1,45 @@
 import { View, StyleSheet } from 'react-native';
-import HomeBtn from './HomeBtn';
-import EndBtn from './EndBtn';
 import RecordBtn from './RecordBtn';
 import PlayBtn from './PlayBtn';
 import RepeatBtn from './RepeatBtn';
-import StopBtn from './StopBtn';
 import PauseBtn from './PauseBtn';
+import TransportIcon from './TransportIcon';
+import TransportIconWithFeedback from './TransportIconWithFeedback';
+import * as icons from './icons.js';
 
 export default function Transport() {
   return (
     <View style={styles.container}>
-      <HomeBtn />
-      <EndBtn />
+      {/* home button */}
+      <TransportIcon
+        icon={icons.home}
+        osc={{ address: '/action', args: [40042] }}
+      />
+
+      {/* end button */}
+      <TransportIcon
+        icon={icons.end}
+        osc={{ address: '/action', args: [40043] }}
+      />
+
+      {/* record button */}
+      <TransportIconWithFeedback
+        icon={icons.stop}
+        osc={{ address: '/record', args: [] }}
+        active={false}
+      />
       <RecordBtn />
+
+      {/* play button */}
       <PlayBtn />
+
+      {/* repeat button */}
       <RepeatBtn />
-      <StopBtn />
+
+      {/* stop button */}
+      <TransportIcon icon={icons.stop} osc={{ address: '/stop', args: [] }} />
+
+      {/* pause button */}
       <PauseBtn />
     </View>
   );
